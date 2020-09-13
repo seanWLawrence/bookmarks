177ad45f995d28f26060e880f93817e834d5e886
Sean W. Lawrence <sean.w.lawrence+programming@gmail.com>
2020-09-12 22:56:55 -0700

    chore(views): create and use dark layout to wrap app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <nav id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/login">About</router-link>
-    </nav>
-    <router-view />
+    <layout>
+      <router-view />
+    </layout>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Layout from "@/components/Layout.vue";
+
+@Component({ components: { Layout } })
+export default class App extends Vue {}
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/Layout.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense>
+        <router-link
+          v-for="link in navigationLinks"
+          :key="link.name"
+          :to="link.to"
+          class="navigation-link"
+        >
+          <v-list-item link>
+            <v-list-item-action>
+              <v-icon>{{ link.icon }}</v-icon>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ link.name }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </router-link>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Bookmarks</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <slot></slot>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String,
+  },
+
+  data: () => ({
+    drawer: null,
+    navigationLinks: [
+      { name: "Dashboard", to: "/", icon: "mdi-view-dashboard" },
+      { name: "Settings", to: "/settings", icon: "mdi-cog" },
+    ],
+  }),
+
+  created() {
+    this.$vuetify.theme.dark = true;
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.navigation-link
+    text-decoration: none
+</style>
